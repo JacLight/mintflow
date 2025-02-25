09825112d2b04830dfa0d2a1b70d3cef70693e26
jaclight <imolewolede@gmail.com>
2025-02-24 21:05:15 -0600

    Add langchain plugin: initialize configuration files, set up TypeScript support, and define plugin structure

---
+++ packages/plugins/langchain/src/index.ts
@@ -0,0 +1,26 @@
+const langchainPlugin = {
+    name: "langchain",
+    icon: "",
+    description: "Description for langchain",
+    id: "langchain",
+    runner: "node",
+    inputSchema: {
+        type: "object",
+
+    },
+    outputSchema: {
+        type: "object",
+    },
+    exampleInput: {
+
+    },
+    exampleOutput: {
+    },
+    documentation: "https://yourdocs.com/langchain",
+    method: "exec",
+    actions: [
+
+    ]
+};
+
+export default langchainPlugin;
