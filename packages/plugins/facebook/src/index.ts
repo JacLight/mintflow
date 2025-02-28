4788ea2f7873fc1105788f7b3e526c25fd808d3a
jaclight <imolewolede@gmail.com>
2025-02-28 01:39:25 -0600

    Add TikTok, YouTube, Facebook, and Instagram plugins: implement plugin structure, configurations, and basic data models

---
+++ packages/plugins/facebook/src/index.ts
@@ -0,0 +1,26 @@
+const facebookPlugin = {
+    name: "facebook",
+    icon: "",
+    description: "Description for facebook",
+    id: "facebook",
+    runner: "node",
+    inputSchema: {
+        type: "object",
+
+    },
+    outputSchema: {
+        type: "object",
+    },
+    exampleInput: {
+
+    },
+    exampleOutput: {
+    },
+    documentation: "https://yourdocs.com/facebook",
+    method: "exec",
+    actions: [
+
+    ]
+};
+
+export default facebookPlugin;
