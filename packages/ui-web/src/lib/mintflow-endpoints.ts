052d7cbfeb19ff24175d6522b7910d77c964dd19
jaclight <imolewolede@gmail.com>
2025-03-25 23:16:11 -0500

    Refactor node retrieval to support field filtering; add mintflow client and endpoints

---
+++ packages/ui-web/src/lib/mintflow-endpoints.ts
@@ -0,0 +1,12 @@
+export const mintflowEndpoints = {
+  get_nodes: {
+    name: 'get_nodes',
+    method: 'get',
+    path: 'api/nodes',
+  },
+  get_node: {
+    name: 'get_node',
+    method: 'get',
+    path: 'api/node',
+  },
+}
